341d69b60597c580479f2ec55ef29c7d1425e662
Daniel <olusakindaniel03@gmail.com>
2025-02-09 18:42:07 +0000

    0.0.3 D1K03 Fixes: OOP Implementation

--- src/main.py
+++ src/main.py
@@ -1,25 +1,31 @@
 import pygame
 import sys
 
-def main():
-    pygame.init()
-    screenResolution = pygame.display.set_mode((800, 800))
-    clock = pygame.time.Clock()
+class Main():
+    def __init__(self):
+        self.isRunning, self.isPlaying = True, True
+        self.screenWidth, self.screenHeight = 1024,768
+        self.display = pygame.Surface((self.screenWidth, self.screenHeight))
+        self.screen = pygame.display.set_mode((self.screenWidth, self.screenHeight))
+        self.clock = pygame.time.Clock()
 
-    isRunning = True
-    while isRunning:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                isRunning = False
-        
+    def run(self):
+        while self.isRunning:
+            self.dt = self.clock.tick(60)/1000
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    self.isRunning = False
+            
+            self.display.fill((100, 25, 0))
+            self.screen.blit(self.display, (0, 0))
+            pygame.display.flip()
 
-        screenResolution.fill((0, 0, 0))
-        pygame.display.flip()
-        clock.tick(60)
-    
-    pygame.quit()
-    sys.exit()
 
 
 if __name__ == "__main__":
-    main()
+    pygame.init()
+    game = Main()
+    game.run()
+    pygame.quit()
+    sys.exit()
+
